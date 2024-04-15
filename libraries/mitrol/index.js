24a5840219b728cbbb8d822663dee2a1f11ee93e
Pedro Lopez Slevin <pedro.lopez@mitrol.net>
2024-04-15 19:45:15 -0300

    trying to fix url

--- libraries/mitrol/index.js
+++ libraries/mitrol/index.js
@@ -177,6 +177,8 @@ return await response.json()
       var copyParam = "";
       var isParam = false;
       var isParamValue = false;
+      console.log(`getUrlParams - formUrl: ${formUrl}`)
+      console.log(`getUrlParams - mitrol.formUrl: ${mitrol.formUrl}`)
       console.log(`getUrlParams - url: ${url}`)
       for(let i = 0; url[i] !== undefined; i++) {
         // Check the word character per character
