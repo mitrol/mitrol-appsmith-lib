4d39e638fca69ec41d4c022b98f82d4912e75865
TomasBruzzesi <tomas.bruzzesi@mitrol.net>
2024-05-24 15:15:48 -0300

    Se modifico setResultadoGestion

--- libraries/mitrol/index.js
+++ libraries/mitrol/index.js
@@ -215,6 +215,7 @@ export default {
       let jwt = String(await mitrol.getUrlParams("jwt"));
       let endpoint = `/api/${loginId}/interactionresult?idInteraccion=${idInteraccion}&crmId=${idCliente}&idResultadoGestionInterno=${idResultadoGestion}&idResultadoGestionExterno=${idResultadoGestion}`
       let response = await mitrol.get(endpoint, jwt)
+      console.log(`setResultadoGestion - ${response}`)
       if (response.status === 200){
         console.log(`setResultadoGestion - success`)
         return true
